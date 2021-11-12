d8c4b6f0f78037d16f88d4636e0723b8ada43884
Amel Dechemi <53280333+TheDech@users.noreply.github.com>
2021-11-11 18:26:40 -0800

    Update motion_planning.py

--- scripts/motion_planning.py
+++ scripts/motion_planning.py
@@ -1,10 +1,16 @@
+def neighbors(current):
+    # define the list of 4 neighbors
+    neighbors = []
+    return [ (current[0]+nbr[0], current[1]+nbr[1]) for nbr in neighbors ]
+
+def heuristic_distance(candidate, goal):
+    pass
+
 def get_path_from_A_star(start, goal, obstacles):
     # input  start: integer 2-tuple of the current grid, e.g., (0, 0)
     #        goal: integer 2-tuple  of the goal grid, e.g., (5, 1)
     #        obstacles: a list of grids marked as obstacles, e.g., [(2, -1), (2, 0), ...]
     # output path: a list of grids connecting start to goal, e.g., [(1, 0), (1, 1), ...]
     #   note that the path should contain the goal but not the start
-    #   e.g., the path from (0, 0) to (2, 2) should be [(1, 0), (1, 1), (2, 1), (2, 2)]
-    
-
+    #   e.g., the path from (0, 0) to (2, 2) should be [(1, 0), (1, 1), (2, 1), (2, 2)] 
     return path
